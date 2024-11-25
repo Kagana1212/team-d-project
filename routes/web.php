86371fcefcc593cee2df3b09989c1b50e999a91b
Kagana1212 <ktc2243030004@edu.kyoto-tech.ac.jp>
2024-11-25 11:37:08 +0900

    11/25 Laravelログイン

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MenuController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
-Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
