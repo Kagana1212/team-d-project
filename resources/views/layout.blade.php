55404da06e6a700c972d9c866825ab5c4ec80ea4
natsuki7 <kotowk.n7@gmail.com>
2024-11-26 12:18:48 +0900

    layout.blade.phpにviewportを追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.min.css">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <title>Lara-Travel</title>
